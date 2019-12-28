8d76f61dc5b59e8a6d109ed1c632d1531624ba47
Bernhard Walser <walserb@student.ethz.ch>
2019-12-28 11:05:22 +0100

    LSTM now working but maybe not learning

--- Models/LSTM.py
+++ Models/LSTM.py
@@ -15,22 +15,21 @@ TAKE_SIZE = 5000
 LSTM_SIZE = 190
 
 # Create Dataset of all FileNames with One Hot Character Encoding
+# and Padding to LSTM_SIZE
 
 all_labeled_data = CreateDataset.create_dataset(FILE_NAMES,LSTM_SIZE)
 
-# Form Batches for train and test data and padd Questions and Answers with Zeros to equal Shapes
+#Split in Train and Test Data (Remove .take(TAKE_SIZE) at train_data to train on all data)
 
-train_data = all_labeled_data.skip(TAKE_SIZE).take(TAKE_SIZE).shuffle(BUFFER_SIZE)
+train_data = all_labeled_data.skip(TAKE_SIZE).take(TAKE_SIZE)
 train_data = train_data.batch(BATCH_SIZE)
 
 test_data = all_labeled_data.take(TAKE_SIZE)
 test_data = test_data.batch(BATCH_SIZE)
 
-#print(next(iter(train_data))[0][0])
-
 model = tf.keras.Sequential()
 model.add(layers.Input(shape=(80, LSTM_SIZE)))
-model.add(layers.LSTM(LSTM_SIZE, return_sequences=True))
+model.add(layers.LSTM(LSTM_SIZE, return_sequences=True, activation='sigmoid'))
 print(model.summary())
 
 optimizer = Adam(
@@ -44,13 +43,17 @@ optimizer = Adam(
 )
 
 model.compile(optimizer=optimizer, loss="categorical_crossentropy")
-    
+
 model.fit(train_data,
           validation_data=test_data,
           epochs=3,
           verbose=1)
 
+#Make sample prediction on one Test data Point
 
-#y_pred = model.predict(x)
-#print(y_pred)
+for x,y in train_data:
+    print(x)git 
+    y_pred = model.predict(x)
+    print(y_pred)
+    break
 
